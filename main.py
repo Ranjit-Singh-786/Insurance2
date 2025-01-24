ecf9e56c2f68a73b1daf462ad9bc77f2ee982b80
Ranjit-singh-786 <jiradhey402@gmail.com>
2025-01-24 11:18:13 +0530

    logging setup done

---
+++ main.py
@@ -0,0 +1,6 @@
+from source_code.logger import logging
+logging.info("hii this is ranjit")
+logging.debug("hii this is ranjit")
+logging.warning("hii this is ranjit")
+logging.error("hii this is ranjit")
+logging.critical("hii this is ranjit")
